29e1521b4f6f3fd826c81977085150d64e48f5d7
Zachary Albia <zach.albia@gmail.com>
2024-01-17 15:41:22 +0800

    update deps

--- project/Libraries.scala
+++ project/Libraries.scala
@@ -2,25 +2,25 @@ import sbt.*
 
 object Libraries {
 
-  val zioVersion       = "2.0.19"
+  val zioVersion       = "2.0.21"
   val slf4jVersion     = "2.0.9"
   val zioConfigVersion = "3.0.7"
   val zioHttpVersion   = "3.0.0-RC4" // "3.0.0-RC4+7-f9ffb0b6-SNAPSHOT"
 
   val zio        = "dev.zio" %% "zio"                % zioVersion
-  val prelude    = "dev.zio" %% "zio-prelude"        % "1.0.0-RC21"
+  val prelude    = "dev.zio" %% "zio-prelude"        % "1.0.0-RC22"
   val zioHttp    = "dev.zio" %% "zio-http"           % zioHttpVersion
-  val zioLogging = "dev.zio" %% "zio-logging-slf4j2" % "2.1.14"
+  val zioLogging = "dev.zio" %% "zio-logging-slf4j2" % "2.1.16"
 
   val tests = Seq(
     "dev.zio" %% "zio-test"          % zioVersion,
     "dev.zio" %% "zio-test-sbt"      % zioVersion,
     "dev.zio" %% "zio-test-magnolia" % zioVersion,
-    "dev.zio" %% "zio-mock"          % "1.0.0-RC11",
+    "dev.zio" %% "zio-mock"          % "1.0.0-RC12",
   )
 
   val loggingRuntime = Seq(
-    "ch.qos.logback"       % "logback-classic"          % "1.4.11",
+    "ch.qos.logback"       % "logback-classic"          % "1.4.14",
     "net.logstash.logback" % "logstash-logback-encoder" % "7.4",
     "org.slf4j"            % "jul-to-slf4j"             % slf4jVersion,
     "org.slf4j"            % "log4j-over-slf4j"         % slf4jVersion,
