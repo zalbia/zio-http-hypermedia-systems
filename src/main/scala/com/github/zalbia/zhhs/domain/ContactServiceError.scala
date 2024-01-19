4421c5a40443ff0fb533900a618570809be8ccd9
Zachary Albia <zach.albia@gmail.com>
2024-01-19 08:44:22 +0800

    simplify ContactServiceError hierarchies

--- src/main/scala/com/github/zalbia/zhhs/domain/ContactServiceError.scala
+++ src/main/scala/com/github/zalbia/zhhs/domain/ContactServiceError.scala
@@ -1,12 +1,12 @@
 package com.github.zalbia.zhhs.domain
 
 // Domain errors for the ContactService
-sealed trait ContactServiceError extends Product with Serializable
 object ContactServiceError {
-  sealed trait SaveContactError   extends ContactServiceError
-  sealed trait UpdateContactError extends ContactServiceError
+  sealed trait SaveContactError   extends Product with Serializable
+  sealed trait UpdateContactError extends Product with Serializable
+  sealed trait DeleteContactError extends Product with Serializable
 
   final case class EmailAlreadyExistsError(email: String)   extends SaveContactError with UpdateContactError
   case object MissingEmailError                             extends SaveContactError with UpdateContactError
-  final case class ContactIdDoesNotExist(contactId: String) extends UpdateContactError
+  final case class ContactIdDoesNotExist(contactId: String) extends UpdateContactError with DeleteContactError
 }
--- src/main/scala/com/github/zalbia/zhhs/domain/UpdateContactDto.scala
+++ src/main/scala/com/github/zalbia/zhhs/domain/UpdateContactDto.scala
@@ -1,5 +1,6 @@
 package com.github.zalbia.zhhs.domain
 
+// A DTO representing an attempt to update an existing contact
 final case class UpdateContactDto(
   contactId: String,
   firstname: Option[String],
